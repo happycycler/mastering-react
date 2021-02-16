04d3951b2b576c89952f373a12abf4660fb0df80
Mark Price <mrprice1@comcast.net>
2021-02-16 18:49:31 -0500

    Updated router-app and vidly.

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import "./App.css";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
@@ -14,10 +16,16 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products" component={Products} />
-            <Route path="/Posts" component={Posts} />
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route
+              path="/products"
+              render={(props) => <Products sortby="newest" {...props} />}
+            />
+            <Route path="/Posts/:year?/:month?" component={Posts} />
             <Route path="/Admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import SideBar from "./sidebar";
+import Users from "./users";
+import Posts from "./posts";
+import { Route, Switch } from "react-router-dom";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <SideBar />
+      <Switch>
+        <Route path="/admin/posts" component={Posts} />;
+        <Route path="/admin/users" component={Users} />;
+      </Switch>
     </div>
   );
 };
