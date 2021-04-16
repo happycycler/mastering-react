9e16b0f160d91bd0be3ba95a0568f4fa2ce676b2
Mark Price <mrprice1@comcast.net>
2021-04-16 09:09:09 -0400

    Added env configuration files.

--- vidly/src/index.js
+++ vidly/src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-console.log(process.env);
+console.log("SUPERMAN", process.env.REACT_APP_NAME);
 
 ReactDOM.render(
   <BrowserRouter>
